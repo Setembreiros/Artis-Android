1b858f67aa35a8bb13513c74761c37defac38634
NewHouses <sergio.simons.c@gmail.com>
2024-10-04 17:53:07 +0200

    [OSP-66] abrir posts a pantalla completa

--- app/src/main/java/com/setembreiros/artis/ui/commponents/Posts.kt
+++ app/src/main/java/com/setembreiros/artis/ui/commponents/Posts.kt
@@ -1,10 +1,16 @@
 package com.setembreiros.artis.ui.commponents
 
 import android.annotation.SuppressLint
+import android.content.ActivityNotFoundException
 import android.content.Context
+import android.content.Intent
 import android.graphics.Bitmap
 import android.graphics.pdf.PdfRenderer
 import android.os.ParcelFileDescriptor
+import android.view.Gravity
+import android.widget.FrameLayout
+import android.widget.ImageButton
+import android.widget.Toast
 import androidx.annotation.OptIn
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
@@ -18,8 +24,14 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -30,8 +42,12 @@ import androidx.compose.ui.graphics.asImageBitmap
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.viewinterop.AndroidView
+import androidx.compose.ui.window.Dialog
+import androidx.compose.ui.window.DialogProperties
+import androidx.core.content.FileProvider
 import androidx.core.net.toUri
 import androidx.media3.common.MediaItem
 import androidx.media3.common.util.UnstableApi
@@ -48,8 +64,8 @@ import java.io.FileOutputStream
 @Composable
 fun PostThumbnail(post: Post, onNavigateToImageDetails: () -> Unit,){
     Box(
-        modifier = Modifier.fillMaxSize(),  // Ensures the box takes up full available space
-        contentAlignment = Alignment.Center // Aligns the play button in the center of the Box
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
     ) {
         when (post.metadata.type) {
             Constants.ContentType.IMAGE -> BasePostThumbnail(
@@ -62,10 +78,10 @@ fun PostThumbnail(post: Post, onNavigateToImageDetails: () -> Unit,){
                     onImageClick = { onNavigateToImageDetails() }
                 )
                 Image(
-                    painter = painterResource(id = R.drawable.play_button),  // Play button icon
+                    painter = painterResource(id = R.drawable.play_button),
                     contentDescription = "Play Button",
-                    modifier = Modifier.size(50.dp),  // Set the size of the play button
-                    colorFilter = ColorFilter.tint(Color.White) // Optional: tint the play button if needed
+                    modifier = Modifier.size(50.dp),
+                    colorFilter = ColorFilter.tint(Color.White)
                 )
             }
             Constants.ContentType.AUDIO -> {
@@ -76,7 +92,7 @@ fun PostThumbnail(post: Post, onNavigateToImageDetails: () -> Unit,){
                     )
                 else {
                     Image(
-                        painter = painterResource(id = R.drawable.audio_default_thumbnail),  // Play button icon
+                        painter = painterResource(id = R.drawable.audio_default_thumbnail),
                         contentDescription = "Audio default thumbnail",
                         modifier = Modifier
                             .shadow(10.dp, RoundedCornerShape(16.dp), clip = true)
@@ -122,6 +138,8 @@ fun BasePostThumbnail(post: Post, onImageClick: () -> Unit){
 
 @Composable
 fun BaseImagePost(post: Post){
+    var isFullScreen by rememberSaveable { mutableStateOf(false) }
+
     AsyncImage(
         model = post.content,
         contentDescription = "Image",
@@ -129,9 +147,32 @@ fun BaseImagePost(post: Post){
             .padding(16.dp)
             .shadow(10.dp, RoundedCornerShape(16.dp), clip = true)
             .height(400.dp)
-            .clip(RoundedCornerShape(16.dp)),
+            .clip(RoundedCornerShape(16.dp))
+            .clickable {
+                isFullScreen = true
+            },
         contentScale = ContentScale.Crop,
     )
+
+    if (isFullScreen) {
+        Dialog(onDismissRequest = { isFullScreen = false },
+            properties = DialogProperties(usePlatformDefaultWidth = false)) {
+            Box(
+                modifier = Modifier
+                    .width(800.dp)
+                    .background(Color.Black)
+                    .clickable { isFullScreen = false }
+            ) {
+                AsyncImage(
+                    model = post.content,
+                    contentDescription = "Full screen image",
+                    modifier = Modifier
+                        .fillMaxSize(),
+                    contentScale = ContentScale.Fit
+                )
+            }
+        }
+    }
 }
 
 @OptIn(UnstableApi::class)
@@ -142,6 +183,9 @@ fun MediaPlayer(post: Post) {
     val videoFile = remember {
         writeByteArrayToFile(context, post.content!!, "temp_media." + post.metadata.fileType)
     }
+
+    var isFullScreen by remember { mutableStateOf(false) }
+
     val exoPlayer = remember {
         ExoPlayer.Builder(context).build().apply {
             val mediaItem = MediaItem.Builder()
@@ -154,42 +198,122 @@ fun MediaPlayer(post: Post) {
         }
     }
 
-    AndroidView(
-        modifier = Modifier
-            .padding(16.dp)
-            .shadow(10.dp, RoundedCornerShape(16.dp), clip = true)
-            .height(400.dp)
-            .clip(RoundedCornerShape(16.dp))
-            .background(Color.Black),
-        factory = {
-            PlayerView(context).apply {
-                player = exoPlayer
-                useController = true
-                resizeMode = AspectRatioFrameLayout.RESIZE_MODE_FIT
-            }
-        },
-    )
-}
+    if (isFullScreen) {
+        Dialog(onDismissRequest = { isFullScreen = false },
+            properties = DialogProperties(usePlatformDefaultWidth = false)) {
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .background(Color.Black)
+            ) {
+                AndroidView(
+                    modifier = Modifier.fillMaxSize(),
+                    factory = {
+                        PlayerView(context).apply {
+                            player = exoPlayer
+                            useController = true
+                            resizeMode = AspectRatioFrameLayout.RESIZE_MODE_FILL
 
+                            val fullscreenButton = ImageButton(context).apply {
+                                setImageResource(android.R.drawable.ic_menu_close_clear_cancel)
+                                setOnClickListener {
+                                    isFullScreen = false
+                                }
+                            }
+
+                            this.addView(fullscreenButton)
+                            val params = FrameLayout.LayoutParams(
+                                FrameLayout.LayoutParams.WRAP_CONTENT,
+                                FrameLayout.LayoutParams.WRAP_CONTENT
+                            ).apply {
+                                gravity = Gravity.END or Gravity.TOP
+                            }
+                            fullscreenButton.layoutParams = params
+                        }
+                    }
+                )
+            }
+        }
+    } else {
+        AndroidView(
+            modifier = Modifier
+                .padding(16.dp)
+                .shadow(10.dp, RoundedCornerShape(16.dp), clip = true)
+                .height(400.dp)
+                .clip(RoundedCornerShape(16.dp))
+                .background(Color.Black),
+            factory = {
+                PlayerView(context).apply {
+                    player = exoPlayer
+                    useController = true
+                    resizeMode = AspectRatioFrameLayout.RESIZE_MODE_FIT
+
+                    val fullscreenButton = ImageButton(context).apply {
+                        setImageResource(android.R.drawable.ic_menu_view)
+                        setOnClickListener {
+                            isFullScreen = true
+                        }
+                    }
+
+                    this.addView(fullscreenButton)
+                    val params = FrameLayout.LayoutParams(
+                        FrameLayout.LayoutParams.WRAP_CONTENT,
+                        FrameLayout.LayoutParams.WRAP_CONTENT
+                    ).apply {
+                        gravity = Gravity.END or Gravity.TOP
+                    }
+                    fullscreenButton.layoutParams = params
+                }
+            }
+        )
+    }
+}
 @Composable
-fun PdfReader(post: Post){
+fun PdfReader(post: Post) {
+    val context = LocalContext.current
+
     val fileDescriptor: ParcelFileDescriptor?
     val pdfRenderer: PdfRenderer?
-    val tempFile: File? = File.createTempFile("temp_pdf", post.metadata.fileType)
-    val fos = FileOutputStream(tempFile)
-    fos.write(post.content)
-    fos.close()
+
+    val tempFile = remember {
+        val file = File.createTempFile("temp_pdf", post.metadata.fileType, context.cacheDir)
+        val fos = FileOutputStream(file)
+        fos.write(post.content)
+        fos.close()
+        file
+    }
 
     fileDescriptor = ParcelFileDescriptor.open(tempFile, ParcelFileDescriptor.MODE_READ_ONLY)
     pdfRenderer = PdfRenderer(fileDescriptor)
 
-    Box(modifier = Modifier
-        .padding(16.dp)
-        .shadow(10.dp, RoundedCornerShape(16.dp), clip = true)
-        .height(400.dp)
-        .background(Color.White)
-        .clip(RoundedCornerShape(16.dp)),
-        contentAlignment = Alignment.Center) {
+    fun openPdfExternally() {
+        val uri = FileProvider.getUriForFile(
+            context,
+            "${context.packageName}.provider",  // Nome do provider
+            tempFile
+        )
+
+        val intent = Intent(Intent.ACTION_VIEW).apply {
+            setDataAndType(uri, "application/pdf")
+            addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION) // Conceder permiso de lectura
+        }
+
+        try {
+            context.startActivity(intent)
+        } catch (e: ActivityNotFoundException) {
+            Toast.makeText(context, "Non hai aplicacións para ler PDFs", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    Box(
+        modifier = Modifier
+            .padding(16.dp)
+            .shadow(10.dp, RoundedCornerShape(16.dp), clip = true)
+            .height(400.dp)
+            .background(Color.White)
+            .clip(RoundedCornerShape(16.dp)),
+        contentAlignment = Alignment.Center
+    ) {
         LazyRow(modifier = Modifier.padding(vertical = 16.dp)) {
             items(count = pdfRenderer.pageCount) { index ->
                 val page = pdfRenderer.openPage(index)
@@ -208,8 +332,17 @@ fun PdfReader(post: Post){
                 page.close()
             }
         }
+
+        Button(
+            onClick = { openPdfExternally() },
+            modifier = Modifier.align(Alignment.BottomCenter)
+        ) {
+            Text(text = stringResource(id = R.string.open_pdf),)
+        }
     }
 }
+
+
 
 
 fun writeByteArrayToFile(context: Context, byteArray: ByteArray, fileName: String): File {
